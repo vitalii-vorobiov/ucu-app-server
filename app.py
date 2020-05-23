568cb5c810dbad110f8ed371fdb5044b89314b98
Vitaliy Vorobyov <vvoro@softserveinc.com>
2020-05-23 15:20:50 +0300

    Updated server

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def verify_password(username_or_token, password):
     return True
 
 
-@app.route('/api/users', methods=['POST'])
+@app.route('/api/register', methods=['POST'])
 def new_user():
     username = request.json.get('username')
     password = request.json.get('password')
@@ -92,22 +92,22 @@ def new_user():
             {'Location': url_for('get_user', id=user.id, _external=True)})
 
 
-@app.route('/api/users/<int:id>')
-def get_user(id):
-    user = User.query.get(id)
-    if not user:
-        abort(400)
-    return jsonify({'username': user.username})
+# @app.route('/api/users/<int:id>')
+# def get_user(id):
+#     user = User.query.get(id)
+#     if not user:
+#         abort(400)
+#     return jsonify({'username': user.username})
 
 
-@app.route('/api/token')
+@app.route('/api/login')
 @auth.login_required
 def get_auth_token():
     token = g.user.generate_auth_token(600)
     return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
 
-@app.route('/api/resource')
+@app.route('/api/events')
 @auth.login_required
 def get_resource():
     return jsonify({'events': events})
